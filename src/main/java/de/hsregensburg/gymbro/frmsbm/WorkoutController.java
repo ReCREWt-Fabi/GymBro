f81479a0376ac673b536e06b6f0b15066285fa16
FScheuermeyer <fabiogattonefg@gmail.com>
2022-11-09 11:48:26 +0100

    Continued working on it

--- src/main/java/de/hsregensburg/gymbro/frmsbm/WorkoutController.java
+++ src/main/java/de/hsregensburg/gymbro/frmsbm/WorkoutController.java
@@ -1,10 +1,19 @@
 package de.hsregensburg.gymbro.frmsbm;
 
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.ModelAndView;
+import java.util.Random;
+
 
 @Controller
 public class WorkoutController {
+
+    private WorkoutPlan user_plan;
 
     @RequestMapping("/")
     public String index() {
@@ -12,13 +21,48 @@ public class WorkoutController {
     }
 
     @RequestMapping("workoutplan_generator")
-    public String workoutplan_generator() {
+    public String workoutplan_generator(
+            @RequestParam(required = false) String workoutplan_name
+    ) {
+        if(workoutplan_name == null) {
+            Random rand = new Random();
+            int n = rand.nextInt(10000);
+            workoutplan_name = "Workout" + Integer.toString(n);
+        }
+        this.user_plan = new WorkoutPlan(workoutplan_name);
         return "workoutplan_generator";
     }
 
-    @RequestMapping("workout_generator")
+    @RequestMapping( value = "workout_generator")
     public String workout_generator() {
         return "workout_generator";
+    }
+
+    @RequestMapping(value = "updated_workoutplan", method = RequestMethod.GET)
+    public ModelAndView added_workout(
+            @RequestParam String wname,
+            @RequestParam String wkdy,
+            @RequestParam int starter,
+            @RequestParam int ender
+    ) {
+        Workout new_workout = new Workout(starter, ender, wkdy, wname);
+        this.user_plan.addWorkout(new_workout);
+        this.user_plan.printWorkouts();
+        return new ModelAndView("workoutplan_generator");
+    }
+
+    @RequestMapping(value = "template_used", method = RequestMethod.GET)
+    public ModelAndView used_template(
+            @RequestParam int template
+    ) {
+        this.user_plan.fillPlanUsingTemplate(template);
+        return new ModelAndView("workoutplan_generator");
+    }
+
+    @RequestMapping(value = "print_plan", method = RequestMethod.GET)
+    public ModelAndView print_plan() {
+        this.user_plan.printWorkouts();
+        return new ModelAndView("workoutplan_generator");
     }
 
 }
--- src/main/java/de/hsregensburg/gymbro/frmsbm/WorkoutPlan.java
+++ src/main/java/de/hsregensburg/gymbro/frmsbm/WorkoutPlan.java
@@ -2,22 +2,79 @@ package de.hsregensburg.gymbro.frmsbm;
 
 public class WorkoutPlan {
 
-    private long id;
-    private String content;
+    private String title;
+    private Workout[] workouts;
+    Workout header = new Workout("Header Element");
 
-    public long getId() {
-        return id;
+    public WorkoutPlan() {
+        this.workouts = new Workout[1];
+        this.workouts[0] = this.header;
     }
 
-    public void setId(long id) {
-        this.id = id;
+    public WorkoutPlan(String t) {
+        workouts = new Workout[1];
+        this.title = t;
+        this.workouts[0] = this.header;
     }
 
-    public String getContent() {
-        return content;
+    public String getTitle() {
+        return title;
     }
 
-    public void setContent(String content) {
-        this.content = content;
+    public void setTitle(String title) {
+        this.title = title;
     }
+
+    public void addWorkout(Workout w) {
+        int n = this.workouts.length;
+        Workout[] new_array = new Workout[n+1];
+        for (int i = 0; i < n; i++)
+            new_array[i] = workouts[i];
+
+        new_array[n] = w;
+        this.workouts = new_array;
+    }
+
+    public void fillPlanUsingTemplate(int id) {
+        if(id == 0001) this.fillPlanUsingLegTemplate();
+        if(id == 0002) this.fillPlanUsingChestTemplate();
+        if(id == 0003) this.fillPlanUsingBackTemplate();
+    }
+
+    private void fillPlanUsingLegTemplate() {
+        Workout a = new Workout(1200, 1300, "Mon", "Leg Workout 1");
+        Workout b = new Workout(1200, 1300, "Wed", "Leg Workout 2");
+        Workout c = new Workout(1200, 1300, "Fri", "Leg Workout 3");
+        this.addWorkout(a);
+        this.addWorkout(b);
+        this.addWorkout(c);
+    }
+
+    private void fillPlanUsingChestTemplate() {
+        Workout a = new Workout(1500, 1600, "Tue", "Chest Workout 1");
+        Workout b = new Workout(1500, 1600, "Thu", "Chest Workout 2");
+        Workout c = new Workout(1500, 1600, "Sat", "Chest Workout 3");
+        this.addWorkout(a);
+        this.addWorkout(b);
+        this.addWorkout(c);
+    }
+
+    private void fillPlanUsingBackTemplate() {
+        Workout a = new Workout(1500, 1600, "Mon", "Back Workout 1");
+        Workout b = new Workout(1500, 1600, "Thu", "Back Workout 2");
+        Workout c = new Workout(1500, 1600, "Sun", "Back Workout 3");
+        this.addWorkout(a);
+        this.addWorkout(b);
+        this.addWorkout(c);
+    }
+
+    public void printWorkouts() {
+        System.out.println(this.title + ":");
+        for (int i = 0; i < this.workouts.length; i++) {
+            System.out.println(this.workouts[i].getTitle());
+        }
+    }
+
 }
+
+
